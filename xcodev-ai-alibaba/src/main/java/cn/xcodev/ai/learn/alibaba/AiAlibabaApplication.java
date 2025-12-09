ec7bcec137d1f6d6c318f1fcad064c965a500794
xiao <xcodev@163.com>
2025-12-09 18:49:31 +0800

    hello world

---
+++ xcodev-ai-alibaba/src/main/java/cn/xcodev/ai/learn/alibaba/AiAlibabaApplication.java
@@ -0,0 +1,17 @@
+package cn.xcodev.ai.learn.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ai alibaba框架启动类
+ *
+ * @author xcodev
+ */
+@SpringBootApplication
+public class AiAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAlibabaApplication.class, args);
+    }
+}
