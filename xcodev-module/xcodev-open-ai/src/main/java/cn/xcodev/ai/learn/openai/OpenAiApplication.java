80194b210cb129c1a182a6cb205677c8b26e07de
xiao <xcodev@163.com>
2025-12-10 11:07:24 +0800

    调整maven结构 添加对openai api的基础使用

---
+++ xcodev-module/xcodev-open-ai/src/main/java/cn/xcodev/ai/learn/openai/OpenAiApplication.java
@@ -0,0 +1,17 @@
+package cn.xcodev.ai.learn.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author xcodev
+ */
+
+@SpringBootApplication
+public class OpenAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAiApplication.class, args);
+    }
+}
